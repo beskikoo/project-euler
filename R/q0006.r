6145f4cf8bd2446fc3cfad018bd56c7918db8265
menjaraz <menjaraz@gmail.com>
2023-02-21 14:29:50 +0300

    Add q0006.r

---
+++ R/q0006.r
@@ -0,0 +1,14 @@
+# Project Euler
+# Question 6
+# Sum Square difference
+# Link: https://projecteuler.net/problem=6
+#
+# Solution of the problem: 25164150
+#
+# Using Programiz R Online Compiler here https://www.programiz.com/r/online-compiler
+#
+# © 2023, menjaraz
+
+answer <- sum(1:100)^2 - sum((1:100)^2)
+
+print(answer)
