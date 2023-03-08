cf666ef7ff5430fb9e0ab5fa2c4ab914b3cc8089
menjaraz <menjaraz@gmail.com>
2023-03-08 22:09:16 +0300

    Add q0034.r

---
+++ R/q0034.r
@@ -0,0 +1,20 @@
+# Project Euler
+# Question 34
+# Largest prime factor
+# Link: https://projecteuler.net/problem=34
+#
+# Solution of the problem: 40730
+#
+# Using R version 4.2.2
+#
+# © 2023, menjaraz
+
+
+answer <- 0
+
+for (n in 3:999999) {
+    digs <- as.numeric(unlist(strsplit(as.character(n), "")))
+    if (sum(factorial(digs)) == n) answer <- answer + n
+}
+
+print(answer)
