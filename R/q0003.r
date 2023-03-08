7442dbc1333563707466c014cc320f3c0ffe8605
menjaraz <menjaraz@gmail.com>
2023-03-08 21:36:56 +0300

    Add q0003.r

---
+++ R/q0003.r
@@ -0,0 +1,15 @@
+# Project Euler
+# Question 3
+# Largest prime factor
+# Link: https://projecteuler.net/problem=3
+#
+# Solution of the problem: 6857
+#
+# Using R version 4.2.2
+#
+# © 2023, menjaraz
+
+
+library(numbers)
+
+print(max(primeFactors(600851475143)))
